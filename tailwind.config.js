5548488738c952df83ecbdec6d5cd065d4505e56
Vitor Ribeiro <vitor.eribeiros@gmail.com>
2022-12-07 14:08:34 -0300

    second commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      'body' : 'Be-Vietnam-Pro',
+    },
     extend: {},
     colors: {
       orangePrimary: "#F25F3A",
